30787a2b4a0505f38e033f02ff171b36682e899a
La Ode Muhammad Abin Akbar <156504300+PeGaSusEng@users.noreply.github.com>
2024-06-04 17:53:32 +0700

    Add files via upload

---
+++ PortofolioJS.js
@@ -0,0 +1,16 @@
+link1 = document.getElementById('bx bxl-linkedin-square');
+link2 = document.getElementById('bx bxl-instagram-alt' );
+link3 = document.getElementById('bx bxl-github');
+
+
+link1.onclick = function(){
+    window.location.href = "https://www.linkedin.com/in/la-ode-muhammad-abin-akbar";
+}
+
+link2.onclick = function(){
+    window.location.href = "https://www.instagram.com/p/C6YAvU2vTE-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==";
+}
+
+link3.onclick = function(){
+    window.location.href = "https://github.com/PeGaSusEng";
+}
